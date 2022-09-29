fd398ea5b970b8ffadd7cffddf6ed4b1869889e6
yurley <yurs.ksf1@gmail.com>
2022-09-28 20:18:49 -0500

    define base templates

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
 ]
