c73eae7e81ae814275f4fe2ec5ea1108c3f98710
yurley <yurs.ksf1@gmail.com>
2022-09-28 20:30:44 -0500

    define view list

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .models import Materia
 # Create your views here.
+
+def get_lista_materias():
+    materias = Materia.objects.all()
+    return materias
+
+def lista_materia(request):
+    template_name = 'materias-list.html'
+    context = {
+        'materias': get_lista_materias()
+    }
+    return render(request, template_name, context)
