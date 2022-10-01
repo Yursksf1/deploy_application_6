948e3129da3a01ad3c1ea03eb1bb5d024febb28a
yurley <yurs.ksf1@gmail.com>
2022-09-30 20:38:35 -0500

    add new field

--- myapp/models.py
+++ myapp/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Materia(models.Model):
     nombre = models.CharField(max_length=100)
+    new_file = models.CharField(max_length=100, default='', blank=True, null=True)
 
     def __str__(self):
         return "{}".format(self.nombre)
